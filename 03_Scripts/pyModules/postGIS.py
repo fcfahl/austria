9b7d61bd0b323c9b3317558b1dbfa67adf355234
fcfahl <fcfahl@gmx.com>
2017-11-19 20:33:24 +0100

    Nov 19 Home: tried to use fk on farms. Problems with join

--- 03_Scripts/pyModules/postGIS.py
+++ 03_Scripts/pyModules/postGIS.py
@@ -270,6 +270,8 @@ def import_CSV_PostGIS (table, csv, sep=','):
         host=db_PostGIS['host'])
     con = sa.create_engine(engine)
 
+    drop_table(table)
+
     #_________read csv:
     df = pd.read_csv(csv, sep=sep, chunksize=100000)
 
@@ -350,6 +352,7 @@ def export_PostGIS_Tables ():
 def restore_PostGIS_Tables ():
 
     db_files = glob.glob(folder['DB'].outDir + '/*.backup')
+    # db_files = ['adm_2017_11_17.backup','farm_2017_11_17.backup']
 
     for inFile  in db_files:
 
