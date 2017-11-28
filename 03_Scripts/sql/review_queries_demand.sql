53a9a9a57910718b2355b3c5afe60fc704c29d6d
fcfahl <fcfahl@gmx.com>
2017-11-28 17:26:41 +0100

    Nov 28 Win end just a missing file

--- 03_Scripts/sql/review_queries_demand.sql
+++ 03_Scripts/sql/review_queries_demand.sql
@@ -1,42 +1,42 @@
-﻿-- select * from optimal_plant_residual_aggr_750kw limit 100;
--- select * from optimal_plant_location_750kw order by id_order limit 100;
--- select * from optimal_plant_links_750kw  limit 100;
+﻿select * from optimal_plant_resources_residual_500kw  where id_target > 101 order by id_target,id_building limit 1000;
+select * from optimal_plant_resources_allocated_500kw   limit 100;
+
+select * from optimal_plant_location_500kw  order by id_order limit 100;
+select * from optimal_plant_links_500kw  where id_target > 101 order by id_target,id_building limit 1000;
+
+select * from optimal_plant_resources_residual_500kw   order by id_building limit 1100;
+
+select * from optimal_plant_residual_aggr_500kw  where id_target > 101 order by id_target limit 1000;
 
 
-  WITH
-        last_record AS (
-            SELECT *
-            FROM optimal_plant_location_750kw
-            ORDER BY id_order DESC
-            LIMIT 1
-        ),
-         manure AS (
-            SELECT *
-            FROM (
-                SELECT a.id_aggregate, a.manure, a.live_methane, b.live_required,
-                SUM (a.live_methane) OVER (PARTITION BY a.id_target ORDER BY a.length ASC) AS live_methane_aggregated,
-                row_number () OVER (ORDER BY a.length ASC) AS live_row
-                FROM optimal_plant_resources_residual_750kw AS a, last_record AS b
-                WHERE a.id_target = b.id_target AND a.live_methane > 0
-                ) AS f, live_columns AS g
-            WHERE live_methane_aggregated <= g.live_aggr + 1 -- grab the next value of the sequence
-        ),
-        crop AS (
-            SELECT *
-            FROM (
-                SELECT a.id_aggregate, a.id_building, a.id_target, a.length, a.crop_production, a.crop_methane, b.crop_required,
-                SUM (a.crop_methane) OVER (PARTITION BY a.id_target ORDER BY a.length ASC) AS crop_methane_aggregated,
-                row_number () OVER (ORDER BY a.length ASC) AS crop_row
-                FROM optimal_plant_resources_residual_750kw AS a, last_record AS b
-                WHERE a.id_target = b.id_target AND a.crop_production > 0
-                ) AS f, crop_columns AS g
-            WHERE f.crop_row <= g.crop_row + 1 -- grab the next value of the sequence
-        ),
-        cost_total AS (
-            SELECT a.id_aggregate, a.cost_harvest, a.cost_ensiling, a.cost_manure, a.cost_total
-            FROM optimal_plant_resources_residual_750kw AS a, crop AS b
-            WHERE a.id_aggregate = b.id_aggregate
+select id_target, sum(live_methane_used) from optimal_plant_links_500kw   group by id_target limit 100;
+select id_target, sum(live_methane) from optimal_plant_residual_500kw   where id_target = 107 group by id_target limit 100;
+select id_target, sum(live_methane) from optimal_plant_resources_allocated_500kw   where id_target = 107 group by id_target limit 100;
+select sum(live_methane) from optimal_plant_resources_allocated_500kw   limit 100;
+
+
+select * from optimal_plant_links_500kw  order by id_building limit 100
+
+
+
+
+select COALESCE(a.live_methane_used,0) - COALESCE(b.live_methane,0)
+from optimal_plant_links_500kw  a, optimal_plant_residual_500kw  b
+where a.id_building = b.id_building  and a.id_target = b.id_target and b.id_target = 107
+
+
+
+join optimal_plant_residual_500kw  b on a.id_building = b.id_building  and a.id_target = b.id_target
+where a.id_building = b.id_building  and a.id_target = b.id_target and b.id_target = 107
+
+
+		SELECT *
+        FROM optimal_plant_residual_aggr_500kw 
+        WHERE methane_demand >= 224000 AND cost_total_aggr > 0  AND rank = 3
+        AND id_target NOT IN (
+            SELECT DISTINCT id_target FROM optimal_plant_location_500kw   -- avoid getting duplicates
         )
-	select * from live_columns
-
-        ;
+        
+        ORDER BY cost_total_aggr ASC
+        LIMIT 1
+            ;
